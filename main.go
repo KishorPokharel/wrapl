ee721591b76fa0789b94229a187a44f30b5709ce
Kishor Pokharel <kishorpokharel7@gmail.com>
2025-12-09 10:28:34 +0545

    refactor command handling to use flags and improve error messages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,19 +12,32 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s '<command with {{}} placeholder>'\n", os.Args[0])
+	var debug bool
+	var commandTemplate string
+	var historyFile string
+
+	flag.BoolVar(&debug, "debug", false, "Enable debug mode")
+	flag.StringVar(&commandTemplate, "command", "", "command with {{}} placeholder")
+	flag.StringVar(&historyFile, "history-file", "", "path to history file")
+	flag.Parse()
+
+	if commandTemplate == "" {
+
+		fmt.Fprintf(os.Stderr, "Error: empty command\n")
+		fmt.Fprintf(os.Stderr, "\nUsage: \n")
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
-
-	template := os.Args[1]
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error getting home directory: %v\n", err)
 		os.Exit(1)
 	}
-	historyFile := filepath.Join(homeDir, ".wrapl_history")
+
+	if historyFile == "" {
+		historyFile = filepath.Join(homeDir, ".wrapl_history")
+	}
 
 	rl, err := readline.NewEx(&readline.Config{
 		Prompt:          "> ",
@@ -53,13 +67,16 @@ func main() {
 			break
 		}
 
-		cmdStr := strings.Replace(template, "{{}}", line, -1)
+		cmdStr := strings.Replace(commandTemplate, "{{}}", line, 1)
+		if debug {
+			fmt.Println("CMD:", cmdStr)
+		}
 		cmd := exec.Command("bash", "-c", cmdStr)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 
 		if err := cmd.Run(); err != nil {
-			fmt.Fprintf(os.Stderr, "Command error: %v\n", err)
+			fmt.Fprintf(os.Stderr, "ERR: %v\n", err)
 		}
 	}
 }
